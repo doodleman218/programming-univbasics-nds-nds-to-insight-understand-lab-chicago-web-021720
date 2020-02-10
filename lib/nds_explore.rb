3578546a8530b89bbd031e69f9ea6a2d00bf4d22
David Wolff <dcwolff218@gmail.com>
2020-02-10 04:49:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -23,16 +23,16 @@ require 'pry'
 
 
 
-# def print_first_directors_movie_titles
+def print_first_directors_movie_titles
   
 
-#   row_index = 0
-#   movies = directors_database[0][:movies]
-#   while row_index < movies.length do
-#   puts movies[row_index][:title]
-#   row_index += 1
+  row_index = 0
+  movies = directors_database[0][:movies]
+  while row_index < movies.length do
+  puts movies[row_index][:title]
+  row_index += 1
   
-#   end
+  end
   
   
-# end
+end
